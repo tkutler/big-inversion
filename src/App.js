ce903999a70ca98f3c36dfaee82aced9cf52ea0f
“Toby <toby@MacBook-Pro.local>
2020-07-19 16:10:08 -0400

    practing using functional components (hooks)

--- src/App.js
+++ src/App.js
@@ -2,23 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import PersonCard from './components/PersonCard'
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard firstName="John" lastName="Smith" age={ 45} hairColor="Brown" /> 
+      <PersonCard firstName="Jane" lastName="Smith" age={ 90 } hairColor="Brown" /> 
+      <PersonCard firstName="Joe" lastName="Gordon" age={ 23 } hairColor="Red" /> 
+      <PersonCard firstName="Maria" lastName="Smith" age={ 40 } hairColor="Brown" /> 
     </div>
   );
 }
